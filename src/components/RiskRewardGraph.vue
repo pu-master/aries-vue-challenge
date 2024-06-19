68eb533ec44a1fa36d8eb078dc805582574516e0
Adel Tahir <pu.master72@gmail.com>
2024-06-19 15:12:36 -0400

    Add max profit/loss, break even points to graph.

--- src/components/RiskRewardGraph.vue
+++ src/components/RiskRewardGraph.vue
@@ -5,6 +5,11 @@
       :options="chartOptions"
       :data="chartData"
     />
+    <ul>
+      <li><strong>Rectangle</strong> indicates max profits.</li>
+      <li><strong>Triangle</strong> indicates max losses.</li>
+      <li><strong>Star</strong> indicates break even points.</li>
+    </ul>
   </div>
 </template>
 
@@ -24,9 +29,10 @@ import {
   ChartDataset,
   Tooltip,
   Legend,
+  PointStyle,
 } from "chart.js";
 import { OptionType } from "@/types";
-import { calculatePL } from "@/helpers";
+import { calculateBreakEvenPoint, calculatePL } from "@/helpers";
 
 ChartJS.register(
   Title,
@@ -58,6 +64,10 @@ export default Vue.extend({
           line: {
             borderWidth: 1,
           },
+        },
+        interaction: {
+          intersect: false,
+          mode: "index",
         },
         plugins: {
           title: {
@@ -95,27 +105,109 @@ export default Vue.extend({
   computed: {
     chartData(): ChartData<"line"> {
       const strikePrices: number[] = [];
-      const datasets: ChartDataset<"line">[] = [];
+      const datasets: ChartDataset<"line" | "scatter">[] = [];
+      // Keep track of max profit, max loss, break even points
+      // for each option.
+      const maxProfits: ({ x: number; y: number } | null)[] = [];
+      const maxLosses: ({ x: number; y: number } | null)[] = [];
+      const breakEvenPoints: number[][] = [];
+
       this.options.forEach((option, index) => {
         strikePrices.push(Number(option.strike_price));
-        datasets.push({ data: [], label: `Option #${index + 1}` });
+
+        maxProfits.push(null);
+        maxLosses.push(null);
+        breakEvenPoints.push([]);
+
+        // Add dataset for each option.
+        datasets.push({
+          data: [],
+          label: `Option #${index + 1}`,
+          pointRadius: [],
+          pointHoverRadius: [],
+          pointStyle: [],
+        });
       });
 
       // Set the range of underlying prices from 0
       // to 2x maximum strike price.
       const maxStrikePrice = Math.max(...strikePrices);
-      const labels: number[] = [];
+      let labels: number[] = [];
+
       for (let price = 0; price <= maxStrikePrice * 2; price += 1) {
         labels.push(price);
-
-        this.options.forEach((option, index) => {
-          datasets[index].data.push(calculatePL(option, price));
-        });
       }
+
+      // Some break even points don't fall into whole number prices,
+      // so we need to add floating number prices for break even points.
+      this.options.forEach((option) => {
+        const breakEvenPoint = calculateBreakEvenPoint(option);
+        if (breakEvenPoint > 0 && !labels.includes(breakEvenPoint)) {
+          labels.push(breakEvenPoint);
+        }
+      });
+
+      // Sort labels in ascending order.
+      labels = labels.sort((a, b) => a - b);
+
+      labels.forEach((price, labelIndex) => {
+        this.options.forEach((option, index) => {
+          const pl = calculatePL(option, price);
+
+          datasets[index].data.push(pl);
+          (datasets[index].pointRadius as number[]).push(1);
+          (datasets[index].pointHoverRadius as number[]).push(4);
+          (datasets[index].pointStyle as PointStyle[]).push("circle");
+
+          // Keep track of break even points.
+          if (pl === 0) {
+            breakEvenPoints[index].push(labelIndex);
+          }
+
+          // Keep track of max profit/loss points.
+          if (maxProfits[index] === null || Number(maxProfits[index]?.y) < pl) {
+            maxProfits[index] = {
+              x: labelIndex,
+              y: pl,
+            };
+          }
+
+          if (maxLosses[index] === null || Number(maxLosses[index]?.y) > pl) {
+            maxLosses[index] = {
+              x: labelIndex,
+              y: pl,
+            };
+          }
+        });
+      });
+
+      // For each max profit/loss point, mark them in a special way.
+      maxProfits.forEach((profit, index) => {
+        const xValue = Number(profit?.x);
+        (datasets[index].pointRadius as number[])[xValue] = 10;
+        (datasets[index].pointHoverRadius as number[])[xValue] = 15;
+        (datasets[index].pointStyle as PointStyle[])[xValue] = "rectRot";
+      });
+
+      maxLosses.forEach((profit, index) => {
+        const xValue = Number(profit?.x);
+        (datasets[index].pointRadius as number[])[xValue] = 10;
+        (datasets[index].pointHoverRadius as number[])[xValue] = 15;
+        (datasets[index].pointStyle as PointStyle[])[xValue] = "triangle";
+      });
+
+      // For each break event point, mark them in a special way.
+      breakEvenPoints.forEach((points, index) => {
+        points.forEach((point) => {
+          (datasets[index].pointRadius as number[])[point] = 10;
+          (datasets[index].pointHoverRadius as number[])[point] = 15;
+          (datasets[index].pointStyle as PointStyle[])[point] = "star";
+        });
+      });
 
       return {
         labels,
-        datasets,
+        datasets: datasets as ChartDataset<"line">[],
       };
     },
   },
@@ -128,5 +220,6 @@ export default Vue.extend({
   margin-left: auto;
   margin-right: auto;
   margin-top: 32px;
+  text-align: left;
 }
 </style>
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,21 +1,46 @@
 import { OptionType } from "./types";
 
+/**
+ * Calculate profit/loss for provided option
+ * and underlying price.
+ */
 export function calculatePL(option: OptionType, underlyingPrice: number) {
+  // We use the `fair value` of the option as `premium` price.
+  const premium =
+    Math.ceil(((Number(option.bid) + Number(option.ask)) / 2) * 100) / 100;
+
+  const strikePrice = Number(option.strike_price);
+
+  let pl;
+  if (option.long_short === "long") {
+    if (option.type === "Call") {
+      pl = Math.max(0, underlyingPrice - strikePrice) - premium;
+    } else {
+      pl = Math.max(0, strikePrice - underlyingPrice) - premium;
+    }
+  } else {
+    if (option.type === "Call") {
+      pl = premium - Math.max(0, underlyingPrice - strikePrice);
+    } else {
+      pl = premium - Math.max(0, strikePrice - underlyingPrice);
+    }
+  }
+
+  // Round to 2 decimal points.
+  return Math.ceil(pl * 100) / 100;
+}
+
+/**
+ * Calculate the break even point for a given option.
+ */
+export function calculateBreakEvenPoint(option: OptionType) {
   // We use the `fair value` of the option as `premium` price.
   const premium = (Number(option.bid) + Number(option.ask)) / 2;
 
   const strikePrice = Number(option.strike_price);
 
-  if (option.long_short === "long") {
-    if (option.type === "Call") {
-      return Math.max(0, underlyingPrice - strikePrice) - premium;
-    }
-    return Math.max(0, strikePrice - underlyingPrice) - premium;
-  }
-
   if (option.type === "Call") {
-    return premium - Math.max(0, underlyingPrice - strikePrice);
+    return premium + strikePrice;
   }
-
-  return premium - Math.max(0, strikePrice - underlyingPrice);
+  return strikePrice - premium;
 }
